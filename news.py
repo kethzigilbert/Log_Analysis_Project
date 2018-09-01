ff4aca052d88ab48c68ccec1523d005e0eee84b0
kethzi <kethzi.gildona@gmail.com>
2018-09-02 00:03:48 +0530

    Added String formatting to Python program

--- news.py
+++ news.py
@@ -5,22 +5,25 @@ import datetime
 import psycopg2
 
 
-def get_popu_three_articles():
+def get_cursor():
     conn = psycopg2.connect(database="news")
     c = conn.cursor()
+    return c
+
+
+def get_popu_three_articles():
+    c = get_cursor()
     c.execute("""Select art.title, ttl.total FROM articles
                  as art, topthreelog as ttl
                  WHERE art.slug=ttl.path1 ORDER BY ttl.total desc""")
     logspop = c.fetchall()
     print "Popular Articles"
     for i in range(0, 3):
-        print '"' + str(logspop[i][0]) + '"' +\
-              " - " + str(logspop[i][1]) + " views"
+        print '"{}" - {} views'.format(logspop[i][0], logspop[i][1])
 
 
 def get_popular_authors():
-    conn = psycopg2.connect(database="news")
-    c = conn.cursor()
+    c = get_cursor()
     c.execute("""SELECT count(aut.name) as autcount, aut.name
                 from linklogart as lla, authors as aut
                 WHERE lla.author=aut.id GROUP BY aut.name
@@ -28,13 +31,11 @@ def get_popular_authors():
     logspop = c.fetchall()
     print "\nPopular Authors"
     for i in range(0, len(logspop)):
-        print '"' + str(logspop[i][1]) + '"'\
-              " - " + str(logspop[i][0]) + " views"
+        print '"{}" - {} views'.format(logspop[i][1], logspop[i][0])
 
 
 def get_error_perc():
-    conn = psycopg2.connect(database="news")
-    c = conn.cursor()
+    c = get_cursor()
     c.execute("""SELECT dr.date, ((de.count / dr.count :: float) * 100)
     :: numeric(10, 2)
     as errorperc FROM daterequests as dr, dateerror as de
@@ -42,10 +43,10 @@ def get_error_perc():
     logspop = c.fetchall()
     print '\nOn which days did more than 1% of requests lead to errors?'
     for i in range(0, len(logspop)):
-        print (logspop[i][0].strftime("%d,%B,%Y")) + " - " +\
-         str(logspop[i][1]) + "%"
+        print '{} - {} %'.format(logspop[i][0].strftime("%d,%B,%Y"),
+                                 logspop[i][1])
 
-
-get_popu_three_articles()
-get_popular_authors()
-get_error_perc()
+if __name__ == '__main__':
+    get_popu_three_articles()
+    get_popular_authors()
+    get_error_perc()
